dabd901c40e088bc8520150ef144b0f3bc70ad18
Will Sobel <will@wvsobel.llc>
2026-03-12 12:32:40 +0100

    Cors initializer only used for testing

--- cors.rb
+++ cors.rb
@@ -1,6 +1,8 @@
+# This file is only for testing, nginx will add cors headers in production
+
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://model.mtconnect.org/'
+    origins 'https://127.0.0.1:3000/'
     resource '/csrf_token', headers: :any, methods: [:get, :options], credentials: true
     resource '/cameo_comment*', headers: :any, methods: [:post, :get, :options], credentials: true
   end
